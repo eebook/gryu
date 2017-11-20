da7b46879d2edc5c4d5d489532c25d04e590044d
knarfeh <knarfeh@outlook.com>
2017-11-20 18:20:38 +0800

    feat: Add url metadata client

--- api/common/clients/__init__.py
+++ api/common/clients/__init__.py
@@ -16,11 +16,13 @@ class CcccRequest(DoRequest):
     headers = GRYU_HEADERS
     target_source = os.getenv('CCCC_SOURCE', 18085)
 
+
 class VhfwRequest(DoRequest):
     endpoint = '{}/{}'.format(os.getenv('VHFW_ENDPOINT', 'http://192.168.199.121:18087'),
                               os.getenv('VHFW_API_VERSION', 'v1'))
     headers = GRYU_HEADERS
     target_source = os.getenv('VHFW_SOURCE', 18087)
+
 
 class HlvsRequest(DoRequest):
     endpoint = '{}/{}'.format(os.getenv('HLVS_ENDPOINT', 'http://192.168.199.121:18086'),
@@ -29,7 +31,29 @@ class HlvsRequest(DoRequest):
     headers = GRYU_HEADERS
     target_source = os.getenv('HLVS_SOURCE', 18086)
 
+
+class HlgdRequest(DoRequest):
+    endpoint = '{}/{}'.format(os.getenv('HLGD_ENDPOINT', 'http://hlgd:80'),
+                              os.getenv('HLGD_API_VERSION', 'v1'))
+    headers = GRYU_HEADERS
+    target_source = os.getenv('HLGD_SOURCE', 18089)
+
+
 class CourierClient(object):
 
     def email(self, _data):
         return HlvsRequest.send('email', method='POST', data=_data)['data']
+
+
+class UrlMetadataClient(object):
+
+    @classmethod
+    def get_url_metadata(cls, _data):
+        result = HlgdRequest.send('url_metadata', method='POST', data=_data)['data']
+        LOGGER.info('Get url_metadata, result: {}'.format(result))
+        return result
+
+    @classmethod
+    def sync_repo_metadata(cls):
+        return HlgdRequest.send('url_metadata/sync', method='PUT')['data']
+
--- api/url_metadata/views.py
+++ api/url_metadata/views.py
@@ -12,6 +12,7 @@ from . import url_metadata_bp
 from .exceptions import UrlMetadataException
 from ..common.utils import json, token_auth
 from ..common.validation import schema
+from ..common.clients import UrlMetadataClient
 
 
 LOGGER = logging.getLogger(__name__)
@@ -22,32 +23,17 @@ LOGGER = logging.getLogger(__name__)
 @schema('get_url_metadata.json')
 @token_auth.login_required
 def get_url_metadata():
-    user = g.user
-    username = user.username
     if request.method == "POST":
         LOGGER.info('Get url metadata')
         data = request.json
-        LOGGER.info('data???{}'.format(data))
-        # TODO: add url metadata client
-        result = dict()
-        if data['url'] == 'http://www.ruanyifeng.com/blog/atom.xml':
-            result['type'] = 'rss'
-            result['info'] = 'TODO'
-            result['github repo'] = 'TODO'
-            result['schema'] = {
-                'properties': {
-                    'gryu': {
-                        'type': 'string',
-                        'description': 'gryu',
-                        'format': 'gryu'
-                    },
-                    'password': {
-                        'type': 'string',
-                        'description': 'Password'
-                    },
-                },
-                'required': ['gryu', 'password']
-            }
-        else:
-            raise UrlMetadataException('url_not_support')
+        result = UrlMetadataClient.get_url_metadata(data)
         return result
+
+
+@url_metadata_bp.route('/sync', methods=['PUT'])
+@json
+@token_auth.login_required
+def sync_url_metadata():
+    if request.method == "PUT":
+        LOGGER.info('Sync url metadata')
+        return UrlMetadataClient.sync_repo_metadata()
