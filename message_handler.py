67a49d5de8c6ae2ae1e8d8a75ed9aa01e682a752
knarfeh <knarfeh@outlook.com>
2018-06-03 22:47:32 +0800

    feat: API design

--- message_handler.py
+++ message_handler.py
@@ -30,6 +30,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URI)
 Session.configure(bind=engine)
 session = Session()
 
+
 def get_user(message):
     """
     if not exist, create user
@@ -44,6 +45,7 @@ def get_user(message):
         username = message.from_user.username + "-tg"
         email = "tg_user@eebook.com"
         user = Users(username=username, email=email, password="nopassword", is_active=True)
+        # TODO: add token
         session.add(user)
         session.commit()
     return user
@@ -60,8 +62,129 @@ def send_welcome(message):
 
 @bot.message_handler(commands=["submit"])
 def submit_url(message):
+    """
+    /submit http://baidu.com
+    """
     print("submit url, message: {}".format(message))
-    bot.reply_to(message, ("test submit"))
+    bot.reply_to(message, "test submit")
+
+
+@bot.message_handler(commands=["url_info"])
+def get_url_info(message):
+    """
+    /url_info http://baidu.com
+    """
+    print("get url info, message: {}".format(message))
+    bot.reply_to(message, "test submit")
+
+
+@bot.message_handler(commands=["supported"])
+def supported_url(message):
+    """
+    Get supported url
+    /supported
+    """
+    print("get supported url, message: {}".format(message))
+    bot.reply_to(message, "test submit")
+
+
+@bot.message_handler(commands=["list"])
+def list_resource(message):
+    """
+    Get a list of job config, job, books
+    /list config
+    /list job
+    /list book
+    """
+    print("get supported url, message: {}".format(message))
+    bot.reply_to(message, "test submit")
+
+@bot.message_handler(commands=["edit update"])
+def update_resource(message):
+    """
+    edit job config, book
+    /edit config
+    /edit book
+    """
+    print("get supported url, message: {}".format(message))
+
+
+@bot.message_handler(commands=["delete"])
+def delete_resource(message):
+    """
+    Delete a resource
+    /delete config
+    /delete job
+    /delete book
+    """
+    pass
+
+
+@bot.message_handler(commands=["detail, get"])
+def get_resource(message):
+    """
+    get details of a resource
+    /detail config
+    /detail job
+    /detail book
+    """
+    pass
+
+
+@bot.message_handler(commands=["logs"])
+def job_logs(message):
+    """
+    get logs of a job
+    /logs                   get logs of latest job
+    /logs job_uuid          get logs of a job
+    /logs 20                get 20 log of latest job
+    /logs job_uuid 20       get 20 logs of a specific job
+    """
+    pass
+
+
+@bot.message_handler(commands=["report"])
+def report_issue(message):
+    """
+    report an issue
+    /report url  report url git repo, git issue
+    /report contact @knarfeh
+    """
+    pass
+
+
+@bot.message_handler(commands=["search"])
+def donate(message):
+    """
+    search books
+    """
+    pass
+
+
+@bot.message_handler(commands=["settings"])
+def donate(message):
+    """
+    settings, including language, search feature(本站搜索，全网搜索)
+    """
+    pass
+
+
+
+@bot.message_handler(commands=["kindle"])
+def kindle(message):
+    """
+    kindle
+    """
+    pass
+
+
+@bot.message_handler(commands=["donate"])
+def donate(message):
+    """
+    """
+    pass
+
+
 
 if __name__ == "__main__":
     from telebot import apihelper
